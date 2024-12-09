8f41eb03a213977065ec1c3f6ebca944bfeccdb7
shwetashekhar98 <48760520+shwetashekhar98@users.noreply.github.com>
2024-12-09 09:50:26 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+DASH_APPS = {
+    "stock_percentage_metrics": 8053,
+    "trend_analysis": 8054,
+    "risk_return_matrix": 8057,
+    "bar_chart_race": 8058,
+    "bubble_chart_animation": 8059,
+    "parallel_coordinates": 8060
+}
+
+TICKERS = ['CVX', 'BA', 'GM', 'C', 'BAC', 'T', 'CAT', 'F', 'DIS', 'DE']
